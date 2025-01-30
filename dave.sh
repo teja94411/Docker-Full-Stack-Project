cbd8c84c11077e4340bdca860986b023507422ba
Teja_Royal <161200555+teja94411@users.noreply.github.com>
2025-01-30 12:05:31 +0530

    Update dave.sh

--- dave.sh
+++ dave.sh
@@ -4,6 +4,7 @@ threshold=80  # No spaces around the equal sign
 
 # Check disk usage and compare with the threshold
 df -h | awk -v thresh=$threshold '$5+0 >= thresh { 
-    system("echo Warning: " $1 " is " $5 " full!") 
+    system("echo Warning: " $1 " is " $5 " full!")
+    echo "completed successfully.."
 }'
 
