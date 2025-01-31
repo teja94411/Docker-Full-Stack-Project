8a84b09a2534814021e10b8f62a166bb4dd23994
Teja_Royal <161200555+teja94411@users.noreply.github.com>
2025-01-31 11:56:17 +0530

    Update dave.sh

--- dave.sh
+++ dave.sh
@@ -5,6 +5,6 @@ threshold=80  # No spaces around the equal sign
 # Check disk usage and compare with the threshold
 df -h | awk -v thresh=$threshold '$5+0 >= thresh { 
     system("echo Warning: " $1 " is " $5 " full!")
-    
+    echo "task completed successfully"
 }'
 echo "Iam a devops engineer"
